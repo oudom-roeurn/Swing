ad25693f34b8a8ca997d1ca54c56f7a5c8437c48
RoeurnOudom <147481615+RoeurnOudom@users.noreply.github.com>
2023-10-10 14:09:23 +0700

    Add files via upload

---
+++ views/lbImage.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package views;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author OUDOM ROEURN
+ */
+class lbImage {
+
+    static void setIcon(ImageIcon imageIcon) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
